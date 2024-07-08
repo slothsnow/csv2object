1b6dcf30ce0d0e38cf4b516c7240f4616a3a7b80
slothsnow <143102784+slothsnow@users.noreply.github.com>
2024-07-08 16:24:10 +0200

    Projektdateien hinzufügen.

---
+++ csv2object/csv2object.cs
@@ -0,0 +1,22 @@
+﻿namespace csv2object
+{
+    public class csv2object
+    {
+        static public List<T> Convert<T>(string csv, char separator)
+        {
+            var table = ConvertCsvToTable(csv, separator);
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The method is used to convert the CSV to a table so it is easier to convert.
+        /// </summary>
+        /// <returns>The method returns the plain CSV file as a table. Rows are the records, columns the fields.</returns>
+        static private List<List<string>> ConvertCsvToTable(string csv, char separator)
+        {
+            return csv.Split("\n") // Split the CSV file at the endings to convert easier
+                      .Select(record => record.Split(separator).ToList()) // Split the records to get fields
+                      .ToList();
+        }
+    }
+}
---
+++ csv2object/object2csv.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Linq;
+
+namespace csv2object
+{
+    public class object2csv
+    {
+        static public string Convert<T>(List<T> objects, [Optional] char separator)
+        {
+            if (objects.Count == 0) return "";
+
+            // Table to convert easier
+            List<List<string>> csv = new List<List<string>>();
+
+            // Get structure of object and put it in the table
+            List<string> names = new List<string>();
+            if (objects[0] is null || objects is null) throw new ArgumentNullException(nameof(objects));
+            names.AddRange(from field in objects[0].GetType().GetProperties()
+                           select field.Name);
+
+            // Copy objects in table
+            foreach (T obj in objects)
+            {
+
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
