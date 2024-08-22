60a8210e2c0dc06fd2c4f9e6a0e8e7d4ae454582
slothsnow <143102784+slothsnow@users.noreply.github.com>
2024-08-22 13:07:51 +0200

    Added test_csv2object.

---
+++ test_csv2object/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace test_csv2object
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
